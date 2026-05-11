6eec5db0f4d34d95218ff1a86ef231e578fb271c
Melody W <melodyvwarfel@gmail.com>
2026-05-10 19:41:10 -0700

    Summary of work:   - mapped inputs for kicking, and joystick controls   - created game manager scene as an autoload.   - added game state information to GameManager script   - implemented a cabinet idle timer   - created basic player scene & script (no movement)   - created UI scene, and basic implementatio of kicks remaining counter.

---
+++ player/player.gd
@@ -0,0 +1,10 @@
+#manages player movement & kicking controls. 
+extends CharacterBody3D
+
+#
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	move_and_slide()
