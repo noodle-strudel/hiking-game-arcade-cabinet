c81f445c61c63a468ec99ed8bcf8b6015ee00241
tristanbay <85427980+tristanbay@users.noreply.github.com>
2026-05-12 09:16:59 -0700

    begin work on rock scene

---
+++ rock/rock.gd
@@ -0,0 +1,16 @@
+extends RigidBody3D
+
+var rock_vertices = PackedVector3Array()
+
+# TODO
+# make rock visual mesh and collision mesh
+# by interpolating between two source meshes
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
