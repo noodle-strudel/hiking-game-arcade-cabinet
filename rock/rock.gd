559d58fd963ef80ea8675670ddcc92a007237e19
tristanbay <85427980+tristanbay@users.noreply.github.com>
2026-05-13 17:03:04 -0700

    Get shape data working for interpolation

--- rock/rock.gd
+++ rock/rock.gd
@@ -1,16 +1,38 @@
 extends RigidBody3D
 
-var rock_vertices = PackedVector3Array()
+# how far along in the interpolation the shape of the rock is
+@export var progress = 0
 
-# TODO
-# make rock visual mesh and collision mesh
-# by interpolating between two source meshes
+# vector arrays for current, starting, and spherical ending rock shapes,
+# respectively
+var current_rock_vertices = PackedVector3Array()
+var starting_rock_vertices = PackedVector3Array()
+var ending_rock_vertices = PackedVector3Array()
+
+# interpolates the entire shape of the rock
+func _interpolate(start: PackedVector3Array, end: PackedVector3Array,\
+		progress: float) -> PackedVector3Array:
+	progress = clamp(progress, 0, 1)
+	var out = PackedVector3Array()
+	for i in range(start.size()):
+		out.push_back(start[i].lerp(end[i], progress))
+	return out
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	starting_rock_vertices = $StartingRockCollision.shape.get_faces()
+	ending_rock_vertices = $SphericalRockCollision.shape.get_faces()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# only change rock shape when kicked
+	if Input.is_action_just_pressed("test_kick"):
+		progress += 0.1
+		if progress > 1:
+			progress = 1
+		print(progress)
+		current_rock_vertices =\
+				_interpolate(starting_rock_vertices, ending_rock_vertices,\
+				progress)
+				
+		$CurrentRockCollision.shape.set_faces(current_rock_vertices)
