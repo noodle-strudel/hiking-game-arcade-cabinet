307c932e41292aa8142d2738d5f226f15e753b11
noodle-strudel <yollaine369@gmail.com>
2026-05-14 22:13:23 -0700

    - fixed rock so it doesnt go through the ground - made the ground in the main scene a box mesh so the rock has less of a chance of falling through - added the rock to the main scene since it was really simple to do - added a test camera besides the player camera to the main scene to test out the rolling rock (press k to make it move and roll!)

--- rock/rock.gd
+++ rock/rock.gd
@@ -1,7 +1,21 @@
 extends RigidBody3D
 
+"""
+Notes from Yollaine:
+I changed the CurrentRockCollision's shape type to be ConvexPolygonShape3D which allows the rock
+to actually collide with the floor rather than just fall through it which is what was happening
+with the ConcavePolygonShape3D.
+
+ConcavePolygonShape3D uses the set_points() function which luckily takes PackedVector3Arrays. So it
+was quite easy to switch over to using that. However, I notice that the mesh's edges are constantly
+appearing and disappearing with each kick interpolation but it doesn't seem to mess with the
+collision of the rock itself. Hopefully this won't become a problem in the future as this is the
+only solution I see working right now.
+"""
+
+
 # how far along in the interpolation the shape of the rock is
-@export var progress = 0
+@export var progress: float = 0.0
 
 # vector arrays for current, starting, and spherical ending rock shapes,
 # respectively
@@ -40,9 +54,15 @@ func _ready() -> void:
 	for i in range(starting_rock_vertices.size()):
 		vertex_dists.push_back(starting_rock_vertices[i].distance_to(\
 				ending_rock_vertices[i]))
+	
+	#TODO: Eventually the starting rock vertices will depend on the number of kicks left which 
+	#perhaps the progress variable can be changed based on that
+	
+	#For now, the rock starts with the starting rock vertices. To be deleted later
+	$CurrentRockCollision.shape.set_points(starting_rock_vertices)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	# when rock is kicked
 	if Input.is_action_just_pressed("test_kick"):
 		progress += 0.01
@@ -54,7 +74,7 @@ func _process(delta: float) -> void:
 		current_rock_vertices =\
 				_interpolate(starting_rock_vertices, ending_rock_vertices,\
 				vertex_dists, progress)
-		$CurrentRockCollision.shape.set_faces(current_rock_vertices)
+		$CurrentRockCollision.shape.set_points(current_rock_vertices)
 		
 		# apply impulse force (kick rock)
 		$".".apply_impulse(_vec_noise(kick_vector, 0.05))
