268d5fe6263aa97dbd8bcacf59c438041dc397d3
Melody W <melodyvwarfel@gmail.com>
2026-05-19 01:28:38 -0700

    tee springarm added to player   - added test keys for force-switching gamestate   - added skeleton code for rock kicking functionality   - added void return values for some previous code in ui/ui.gd

--- player/player.gd
+++ player/player.gd
@@ -1,10 +1,40 @@
 #manages player movement & kicking controls. 
 extends CharacterBody3D
 
+
+func _ready() -> void:
+	GameManager.gamestate_update.connect(_on_change_state)
+	
 #
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	#handle kicking rock
+	if GameManager.state == GameManager.gamestates.KICKING:
+		pass
+		#TODO: update the rock's position
 
 	move_and_slide()
+
+func _on_change_state(state: GameManager.gamestates) -> void:
+	match state:
+		GameManager.gamestates.IDLE:
+			pass
+		GameManager.gamestates.CONTRACT:
+			pass
+		GameManager.gamestates.KICKING:
+			_begin_kicking()
+		GameManager.gamestates.ROCK_KICKED:
+			_kick_rock()
+		GameManager.gamestates.SCORING:
+			pass
+
+func _begin_kicking() -> void:
+	#TODO move the player to the rock
+	pass
+
+func _kick_rock() -> void:
+	pass
+	#TODO handle kicking rock
