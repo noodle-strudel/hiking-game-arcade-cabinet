46e059e6ab353684d2be8315735031b5f68eb430
noodle-strudel <yollaine369@gmail.com>
2026-05-24 16:17:54 -0700

    added heaven stairs and changed rock

--- level_grids/park/park_grid.gd
+++ level_grids/park/park_grid.gd
@@ -7,15 +7,15 @@ extends Node3D
 # static body collision shape for the trees
 @onready var conifer_collision_body: PackedScene = preload("res://level_grids/park/trees/conifer_tree_collision_body.tscn")
 
-"""PUBLIC VARIABLES SINCE THE PARK GRID ACTS LIKE A CONTAINER OF INFO LIKE A NODE"""
-var top: Vector3 = Vector3(0, 0, -555.845)
-var left: Vector3 = Vector3(-415.146, 0, 0)
-var right: Vector3 = Vector3(424.854, 0, 0)
-var bottom: Vector3 = Vector3(0, 0, 534.155)
-var top_left: Vector3 = top + left
-var top_right: Vector3 = top + right
-var bottom_left: Vector3 = bottom + left
-var bottom_right: Vector3 = bottom + right
+"""PUBLIC CONSTANTS SINCE THE PARK GRID ACTS LIKE A CONTAINER OF INFO LIKE A NODE"""
+const top: Vector3 = Vector3(0, 0, -555.845)
+const left: Vector3 = Vector3(-415.146, 0, 0)
+const right: Vector3 = Vector3(424.854, 0, 0)
+const bottom: Vector3 = Vector3(0, 0, 534.155)
+const top_left: Vector3 = top + left
+const top_right: Vector3 = top + right
+const bottom_left: Vector3 = bottom + left
+const bottom_right: Vector3 = bottom + right
 
 
 # Called when the node enters the scene tree for the first time.
