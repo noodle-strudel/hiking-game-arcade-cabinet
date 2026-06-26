eb1977cfacd197a49d7401753e612a23f7e679bc
unknown <mikaram@pdx.edu>
2026-06-26 12:30:48 -0700

    Refactor on audio manager, database manager and save load scripts

--- game_manager/save_load.gd
+++ game_manager/save_load.gd
@@ -1,25 +1,25 @@
 extends Node
 
-#Saves are made to the Godot appdata folder in app_userdata
+# Saves are made to the Godot appdata folder in app_userdata
 const save_location = "user://SaveFile.json"
 
-#Dictionary allows for adding more variables to save easily
+# Dictionary allows for adding more variables to save easily
 var content_to_save : Dictionary = {
 	"remaining_kicks" : 0
 }
 
-#Loads the save on game startup
+# Loads the save on game startup
 func _ready() -> void:
 	GameManager.decrement_kicks_remaining.connect(_on_kick_save)
 	_load()
 
-#Save function opens the file and writes the data to save to it
+# Save function opens the file and writes the data to save to it
 func _save() -> void:
 	var file = FileAccess.open(save_location, FileAccess.WRITE)
 	file.store_var(content_to_save.duplicate())
 	file.close()
 
-#If the save file exists this gets the data in there and sets the global kicks remaining
+# If the save file exists this gets the data in there and sets the global kicks remaining
 func _load() -> void:
 	if FileAccess.file_exists(save_location):
 		var file = FileAccess.open(save_location, FileAccess.READ)
