2190ef02f92fa92b36e6116e9535cb727c0a4613
Melody W <melodyvwarfel@gmail.com>
2026-06-26 20:19:45 -0700

    code cleanup for several scripts:   - event_base.gd   - game_manager.gd   - main.gd

--- events/event_base.gd
+++ events/event_base.gd
@@ -1,31 +1,39 @@
-#base class for events to occur. Provides a template for making events
 @icon("res://events/event_icon.png")
 class_name Event extends Node
-#to use, create a node of type "Event" and define a _event_function() function in its script. 
+## Base class for events to occur. Provides a template for making events.
+# to use, create a node of type "Event" and define a _event_function() function in its script. 
 
-#signal for when the event has completed
+# signal for when the event has completed
 signal event_done
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	#connect signals
-	event_done.connect(_event_cleanup)
-	#pause the game
-	get_tree().paused = true
-	#kick off script
-	_event_function()
-	#clean up
-	#_event_cleanup()
-
-#IF YOU WISH TO CREATE AN EVENT:
-#create a script for that extends "Event", and define this function in it. The event_manager will handle
-#kicking this off
+# IF YOU WISH TO CREATE AN EVENT:
+# create a script for that extends "Event", and define this function in it. The event_manager will handle
+# kicking this off
+## Define all event logic in this function for derived scripts.
 func _event_function() -> void:
 	pass
 
-#should be called as soon as the event is done. Resumes normal gameplay and cleans itself up
+
+# should be called as soon as the event is done. Resumes normal gameplay and cleans itself up
+## Must be called by deriving scripts at the end of the event.
 func _event_cleanup() -> void:
-	#unpause the game
+	# unpause the game
 	get_tree().paused = false
-	#prune self from tree
+	
+	# rune self from tree
 	self.queue_free()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# connect signals
+	event_done.connect(_event_cleanup)
+	
+	# pause the game
+	get_tree().paused = true
+	
+	# kick off script
+	_event_function()
+	
+	# clean up
+	#_event_cleanup()
