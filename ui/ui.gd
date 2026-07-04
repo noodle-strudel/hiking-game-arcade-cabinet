e09d0f1b2f59379e3433e659549de40982d53d03
Melody W <melodyvwarfel@gmail.com>
2026-07-04 00:44:58 -0700

    fixed scoring sequence going back to idle

--- ui/ui.gd
+++ ui/ui.gd
@@ -111,6 +111,7 @@ func _scoring_sequence() -> void:
 	AudioManager.play_track(AudioManager.game_over_music)
 	await get_tree().create_timer(50).timeout
 	AudioManager.fade_out_music()
+	GameManager.switch_state_to(GameManager.gamestates.IDLE, "scoring sequence finished")
 
 
 func game_over_text_reset() -> void:
