71487346852fc7ab0415bbec76782affe321f870
Melody W <melodyvwarfel@gmail.com>
2026-07-04 00:57:57 -0700

    animated the grandma salmon entering the frame

--- events/grandma_salmon/grandma_salmon.gd
+++ events/grandma_salmon/grandma_salmon.gd
@@ -16,23 +16,23 @@ func _event_function() -> void:
 	
 	# mouth movement timings
 	await get_tree().create_timer(2.4).timeout
-	$GrandmaHead.pause()
+	%GrandmaHead.pause()
 	await get_tree().create_timer(0.25).timeout
-	$GrandmaHead.play()
+	%GrandmaHead.play()
 	await get_tree().create_timer(2).timeout
-	$GrandmaHead.pause()
+	%GrandmaHead.pause()
 	await get_tree().create_timer(0.3).timeout
-	$GrandmaHead.play()
+	%GrandmaHead.play()
 	await get_tree().create_timer(0.8).timeout
-	$GrandmaHead.pause()
+	%GrandmaHead.pause()
 	await get_tree().create_timer(0.45).timeout
-	$GrandmaHead.play()
+	%GrandmaHead.play()
 	await get_tree().create_timer(1.2).timeout
-	$GrandmaHead.pause()
+	%GrandmaHead.pause()
 	await get_tree().create_timer(0.25).timeout
-	$GrandmaHead.play()
+	%GrandmaHead.play()
 	await get_tree().create_timer(1.6).timeout
-	$GrandmaHead.pause()
+	%GrandmaHead.pause()
 	await get_tree().create_timer(1.75).timeout
 	
 	_event_cleanup()
