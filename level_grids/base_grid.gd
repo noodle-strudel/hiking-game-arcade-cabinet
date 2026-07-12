75a25677c2898f7ed549e80b4606c538d417210b
noodle-strudel <yollaine369@gmail.com>
2026-07-11 18:39:59 -0700

    caught a bug where adding and deleting multimeshes collisions caused a crash and made sure the instances were valid before trying to delete them. also tried to do a queue sort of thing when quickly adding and deleting multimesh collisions

--- level_grids/base_grid.gd
+++ level_grids/base_grid.gd
@@ -1,6 +1,9 @@
 extends Node3D
 class_name Grid
 
+signal modify_multimesh_complete
+var modifying_multimeshes: bool = false
+
 ## The node that will hold all the StaticBody3D nodes.
 @export var object_collisions_node: Node3D
 
@@ -12,20 +15,34 @@ class_name Grid
 # shapes to a single staticbody3d. Plus i'm able to add the transform to the 
 # staticbody3d much easier than a collisionshape3d.
 func add_collision_to_multimeshes() -> void:
+	_await_modification()
+	modifying_multimeshes = true
 	if object_collisions_node and !object_collisions_node.get_children():
 		for mulmesh in mulmesh_col_dict:
 			for i in range(mulmesh.instance_count):
 				var collision: StaticBody3D = mulmesh_col_dict[mulmesh].instantiate()
 				collision.set_transform(mulmesh.get_instance_transform(i))
 				object_collisions_node.add_child(collision)
+	modifying_multimeshes = false
+	modify_multimesh_complete.emit()
 
 func remove_collisions_from_multimeshes() -> void:
+	_await_modification()
+	modifying_multimeshes = true
 	if object_collisions_node:
 		for child in object_collisions_node.get_children():
-			child.queue_free()
+			if is_instance_valid(child):
+				child.queue_free()
 			
 			# stagger queue_free to avoid lag spike
 			await get_tree().physics_frame
+	modifying_multimeshes = false
+	modify_multimesh_complete.emit()
+
+func _await_modification() -> void:
+	if modifying_multimeshes:
+		print("GRID: multimesh deletion occuring, please wait")
+		await modify_multimesh_complete
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
