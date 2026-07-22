8eff3f6041409ad4fe858c7b46fb0df913e0379c
noodle-strudel <yollaine369@gmail.com>
2026-07-21 21:31:23 -0700

    worked on borb more and added first version of purgatory grid

--- levels/main/main.gd
+++ levels/main/main.gd
@@ -12,14 +12,15 @@ signal rock_followcam_activated
 
 ## List of scenes that can be instantiated as chunks
 @onready var _level_segments = [
-	preload("res://level_grids/park/park_grid.tscn"),
+	#preload("res://level_grids/park/park_grid.tscn"),
 	#preload("res://level_grids/heaven/heaven_grid.tscn"),
-	#preload("res://level_grids/city/city_grid.tscn")
+	#preload("res://level_grids/city/city_grid.tscn"),
+	preload("res://level_grids/purgatory/surrounding_grids/purgatory_1_grid.tscn")
 ]
 
 ## List of scenes that can be instantiated as chunks when the number of kicks go down
 @onready var _low_kick_level_segments = [
-	preload("res://level_grids/heaven_stairs/heaven_stairs_grid.tscn"),
+	preload("res://level_grids/purgatory/heaven_stairs_grid.tscn"),
 ]
 
 @onready var grandma_salmon_helper_scene: PackedScene = preload(
