9be16e32b49e6700e95295e0bdb65e1f0aff4b94
Alice <aliceloeb03@gmail.com>
2026-07-29 00:29:24 -0700

    The Sign

--- level_grids/purgatory/heaven_stairs_grid.gd
+++ level_grids/purgatory/heaven_stairs_grid.gd
@@ -9,4 +9,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$SignHelper/SubViewport/SignText.text = "Until\nThe Change:\n" \
+	+ str($"/root/GameManager".kicks_remaining - 8000)
