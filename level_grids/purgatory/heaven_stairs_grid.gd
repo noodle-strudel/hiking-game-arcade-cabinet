cadde6121bc1d8bc68dd118bbf714a42108ea233
Alice <aliceloeb03@gmail.com>
2026-07-30 06:26:12 -0700

    sign repaired, ui updated.

--- level_grids/purgatory/heaven_stairs_grid.gd
+++ level_grids/purgatory/heaven_stairs_grid.gd
@@ -1,13 +1,19 @@
 extends Grid
 class_name HeavenStairsGrid
 
+var purgatory_kicks = GameManager.kicks_remaining - 8000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
-
+	purgatory_kicks = GameManager.kicks_remaining - 8000
+	$SignHelper/SubViewport/SignText.text = "Until\nThe Change:\n" + str(purgatory_kicks)
+	GameManager.decrement_kicks_remaining.connect(_on_update_kicks_remaining)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$SignHelper/SubViewport/SignText.text = "Until\nThe Change:\n" \
-	+ str($"/root/GameManager".kicks_remaining - 8000)
+	pass
+
+func _on_update_kicks_remaining(kick_count: int) -> void:
+	purgatory_kicks = kick_count - 8000
+	$SignHelper/SubViewport/SignText.text = "Until\nThe Change:\n" + str(purgatory_kicks)
