3b6d7815cb9cdb6f1a18a2ce7202fa54eff74ce2
Melody W <melodyvwarfel@gmail.com>
2026-08-12 03:43:00 -0700

    credits fix, theme, and spacing fix

--- ui/credits/credit.gd
+++ ui/credits/credit.gd
@@ -1,9 +1,6 @@
 class_name Credit extends Label
 
 ## float from 0 to 1 indicating how much of the text is visible
-@export var progress := 0.0
-var character_count : int = 0
-
 
 # 
 func write() -> void:
@@ -12,13 +9,9 @@ func write() -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	character_count = _get_character_length()
-	self.visible_characters = 0
+	self.visible_ratio = 0.0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	self.visible_characters = int(progress * character_count)
-
-func _get_character_length() -> int:
-	return self.text.length()
+	pass
