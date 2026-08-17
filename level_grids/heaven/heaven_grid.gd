62dab3349fd048d1df3f656e8929adb4e4b9e6d7
noodle-strudel <yollaine369@gmail.com>
2026-08-16 23:19:17 -0700

    added final changes to city and added barriers + player falling failsafe to heaven grid

--- level_grids/heaven/heaven_grid.gd
+++ level_grids/heaven/heaven_grid.gd
@@ -15,6 +15,10 @@ func _process(delta: float) -> void:
 func _on_oob_barrier_body_entered(body: Node3D) -> void:
 	if body.name == "Rock":
 		GameManager.switch_state_to(GameManager.gamestates.ROCK_OOB, "The rock fell through the ground...")
+	
+	# failsafe to just reload the entire game
+	if body.name == "Player":
+		get_tree().reload_current_scene()
 
 
 func _on_lake_barrier_body_entered(body: Node3D) -> void:
