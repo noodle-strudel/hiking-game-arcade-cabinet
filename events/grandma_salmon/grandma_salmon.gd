2a78256286e3afbfaef6ddc7b867645c0b3aa8df
tristanbay <85427980+tristanbay@users.noreply.github.com>
2026-08-21 13:36:09 -0700

    remove old grandma salmon mouth movement timing code

--- events/grandma_salmon/grandma_salmon.gd
+++ events/grandma_salmon/grandma_salmon.gd
@@ -14,28 +14,6 @@ func _process(_delta: float) -> void:
 
 func _event_function() -> void:
 	%GrandmaHead.play()
-	"""
-	# mouth movement timings
-	await get_tree().create_timer(2.4).timeout
-	%GrandmaHead.pause()
-	await get_tree().create_timer(0.25).timeout
-	%GrandmaHead.play()
-	await get_tree().create_timer(2).timeout
-	%GrandmaHead.pause()
-	await get_tree().create_timer(0.3).timeout
-	%GrandmaHead.play()
-	await get_tree().create_timer(0.8).timeout
-	%GrandmaHead.pause()
-	await get_tree().create_timer(0.45).timeout
-	%GrandmaHead.play()
-	await get_tree().create_timer(1.2).timeout
-	%GrandmaHead.pause()
-	await get_tree().create_timer(0.25).timeout
-	%GrandmaHead.play()
-	await get_tree().create_timer(1.6).timeout
-	%GrandmaHead.pause()
-	await get_tree().create_timer(1.75).timeout
-	"""
 	await $GSSpeech.finished
 	%GrandmaHead.pause()
 	_event_cleanup()
